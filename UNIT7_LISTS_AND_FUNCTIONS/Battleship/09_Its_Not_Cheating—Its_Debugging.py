60fb4e0e6615d808b035f2e957ad686043ca4606
Naoya Hashimoto <nhashimo@redhat.com>
2016-01-27 22:59:50 +0900

    Finished Battleship

--- UNIT7_LISTS_AND_FUNCTIONS/Battleship/09_Its_Not_Cheating—Its_Debugging.py
+++ UNIT7_LISTS_AND_FUNCTIONS/Battleship/09_Its_Not_Cheating—Its_Debugging.py
@@ -2,22 +2,22 @@ from random import randint
 
 board = []
 
-for i in range(5):
-    board.append(["O"]*5)
+for x in range(0,5):
+    board.append(["O"] * 5)
 
 def print_board(board):
     for row in board:
-      print " ".join(row)
+        print " ".join(row)
 
 def random_row(board):
-    return randint(0, len(board) - 1)
+    return randint(0, len(board[0]) - 1)
 
 def random_col(board):
-    return randint(0, len(board) - 1)
+    return randint(0, len(board[0]) - 1)
 
 ship_row = random_row(board)
 ship_col = random_col(board)
 
 # Add your code below!
-print ship_col
 print ship_row
+print ship_col
